b14bdd1bcf7a5efc3f0750f4851f64dc5e351f7a
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-06-04 23:28:04 +0500

    skills updated

--- src/models/skills.model.js
+++ src/models/skills.model.js
@@ -4,6 +4,7 @@ const skillsModel = new mongoose.Schema({
   name: { type: String, required: true },
   logo: { type: String, required: true },
   proficiency: { type: Number, required: true },
+  catergory: { type: String, required: true },
 });
 
 const Skills = mongoose.model("skills", skillsModel);
