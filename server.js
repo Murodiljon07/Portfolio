4e87c326874cfc022fc577e129decee26ba56099
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-06-05 01:38:12 +0500

    server off

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import http from "http";
-import app from "./src/app.js";
+// import http from "http";
+// import app from "./src/app.js";
 
-const server = http.createServer(app);
-const PORT = 8080;
+// const server = http.createServer(app);
+// const PORT = 8080;
 
-server.listen(PORT, () => {
-  console.log(`Server is running PORT:${PORT}\nhttp://localhost:${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`Server is running PORT:${PORT}\nhttp://localhost:${PORT}`);
+// });
